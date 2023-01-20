e181cce903506121b8d582e842deaf42cbb6e4b1
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-01-20 22:09:42 +0200

    add propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FeedbackOptions } from './Feedback/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { SectionTitle } from './SectionTitle/SectionTitle';
+import { Title } from './Title/Title';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    visible: false,
+  };
+
+  onLeaveFeedback = evt => {
+    this.showStatistics();
+    const name = evt.target.name;
+    this.setState(prevState => {
+      return { [name]: (prevState[name] += 1) };
+    });
+  };
+
+  showStatistics = () => {
+    this.setState({ visible: true });
+  };
+
+  countTotalFeedback = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const percentage =
+      +Math.round(
+        (this.state.good * 100) /
+          (this.state.good + this.state.neutral + this.state.bad)
+      ) + '%';
+    return percentage;
+  };
+
+  render() {
+    return (
+      <>
+        <Title />
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        <SectionTitle />
+        
+        {this.state.visible ? (
+          <Statistics
+            state={this.state}
+            countTotalFeedback={this.countTotalFeedback}
+            countPositiveFeedbackPercentage={
+              this.countPositiveFeedbackPercentage
+            }
+          />
+        ) : <Notification />}
+      </>
+    );
+  }
+}
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+import css from './SectionTitle.module.css';
+
+export const SectionTitle= () => {
+    return (
+      <div className={css.container}>
+        <h2 className={css.title}>Statistics</h2>
+      </div>
+    );
+  };
+  
